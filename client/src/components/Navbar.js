5fa709c3c0df07f2ddd4917ee1370b2848aea0b3
Clara <clara.schramm94@googlemail.com>
2022-05-09 21:29:06 +0200

    third commit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import {Button} from "./Button";
 import "./Navbar.css";
 
 function Navbar() {
+  const[click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () =>setClick(false);
+
+const showButton = () => {
+  if (window.innerWidth <=960) {
+    setButton(false);
+  }else {
+    setButton(true);
+  }
+};
+
+window.addEventListener("resize",showButton);
+
   return (
     <>
       <nav className="navbar"></nav>
@@ -10,6 +27,37 @@ function Navbar() {
         <Link to="/" className="navbar-logo">
           I LOVE MY PET <i className="fab fa-typo3"></i>
         </Link>
+        <div className="menu-icon" onClick={handleClick}> 
+        <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+        </div>
+        <ul className = {click ? "nav-menu active" : "nav-menu"}>
+        <li className="nav-items">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Home
+          </Link>
+        </li>
+        <li className="nav-items">
+          <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
+            LogIn
+          </Link>
+        </li>
+        <li className="nav-items">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Profile
+          </Link>
+        </li>
+        <li className="nav-items">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Content
+          </Link>
+        </li>
+        <li className="nav-items">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Upload
+          </Link>
+        </li>
+        </ul>
+        {button && <Button buttonStyle="btn--outline">Log In</Button>}
       </div>
     </>
   );
